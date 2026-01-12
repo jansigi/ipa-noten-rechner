17068ea253e46b47b5188b755933e19e95f443bb
fliberto <fabio.liberto@ergon.ch>
2026-01-12 16:37:51 +0100

    Add backend tests

---
+++ backend/src/main/kotlin/ch/jf/ipa/dto/EvaluatedCriterionDto.kt
@@ -0,0 +1,11 @@
+package ch.jf.ipa.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EvaluatedCriterionDto(
+    val criterionId: String,
+    val totalRequirements: Int,
+    val checkedRequirements: Int,
+    val grade: Int,
+)
